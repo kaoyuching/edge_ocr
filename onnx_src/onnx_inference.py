68c17f92d2cbf391770d3beecd877599ce16a99b
doriskao <doris_kao@muenai.com>
2024-06-04 18:26:47 +0800

    UPDATE: run onnx on GPU

--- onnx_src/onnx_inference.py
+++ onnx_src/onnx_inference.py
@@ -9,25 +9,42 @@ import time
 # import atexit
 import onnx
 import onnxruntime
+from cuda import cudart
 
 from utils.image_utils import load_detect_image, decode_box, load_ocr_image
 from utils.ocr_utils import ctc_decode, text_add_dash
 
 
 class OnnxInference:
-    def __init__(self, engine_filepath: str, device: int = 0):
-        # set_device_success = cudart.cudaSetDevice(device)
+    def __init__(self, engine_filepath: str, device: int = -1):
+        self.device = device
         self.session = self._load_engine(engine_filepath)
 
     def _load_engine(self, engine_filepath: str):
         assert os.path.exists(engine_filepath)
+        if self.device >= 0:
+            providers = [
+               ('CUDAExecutionProvider', {'device_id': self.device},), 
+               'CPUExecutionProvider'
+            ]
+        else:
+            providers = ['CPUExecutionProvider']
+
         session = onnxruntime.InferenceSession(
             engine_filepath,
-            # providers=['CUDAExecutionProvider', 'CPUExecutionProvider'],
+            providers=providers,
         )
         return session
 
     def run(self, input_data: List[np.ndarray]):
+        set_device_success = cudart.cudaSetDevice(self.device)
+        if self.device == -1:
+            outputs = self._run_cpu(input_data)
+        else:
+            outputs = self._run_gpu(input_data)
+        return outputs
+
+    def _run_cpu(self, input_data: List[np.ndarray]):
         if not isinstance(input_data, list):
             input_data = [input_data]
         session_inputs = self.session.get_inputs()
@@ -38,30 +55,53 @@ class OnnxInference:
         outputs = self.session.run(None, data)
         return outputs
 
+    def _run_gpu(self, input_data: List[np.ndarray]):
+        if not isinstance(input_data, list):
+            input_data = [input_data]
+        input_data_ort = [onnxruntime.OrtValue.ortvalue_from_numpy(x, 'cuda', self.device) for x in input_data]
+        session_inputs = self.session.get_inputs()
+        io_binding = self.session.io_binding()
+        for i, sess in enumerate(session_inputs):
+            # io_binding.bind_cpu_input(sess.name, input_data[i])
+            io_binding.bind_input(
+                name=sess.name, 
+                device_type=input_data_ort[i].device_name(),
+                device_id=self.device,
+                element_type=np.float32,
+                shape=input_data_ort[i].shape(),
+                buffer_ptr=input_data_ort[i].data_ptr()
+            )
+        io_binding.bind_output('output', device_type='cuda', device_id=self.device)
+        self.session.run_with_iobinding(io_binding)
+        outputs = io_binding.copy_outputs_to_cpu()
+        return outputs
+
 
 if __name__ == '__main__':
     CHARS = '0123456789abcdefghijklmnopqrstuvwxyz'
     LABEL2CHAR = {i + 1: char for i, char in enumerate(CHARS)}
 
     st = time.time()
-    yolov5_infer = OnnxInference('/home/doriskao/workspace/test_edge/onnx_models/yolov5s.onnx', device=1)
+    device = -1
+    detect_infer = OnnxInference('/home/doriskao/workspace/test_edge/onnx_models/yolov5s.onnx', device=device)
     print('load yolov5')
-    nms_infer = OnnxInference('/home/doriskao/workspace/test_edge/onnx_models/yolov5_post_nms_xyxy_single.onnx', device=1)
+    nms_infer = OnnxInference('/home/doriskao/workspace/test_edge/onnx_models/yolov5_post_nms_xyxy_single.onnx', device=device)
     print('load nms')
-    crnn_infer = OnnxInference('/home/doriskao/workspace/test_edge/onnx_models/crnn.onnx', device=1)
+    crnn_infer = OnnxInference('/home/doriskao/workspace/test_edge/onnx_models/crnn.onnx', device=device)
     print('load sessions:', time.time() - st)
 
     img_paths = [os.path.join("/data/data_set/doriskao/ocr_dataset/car_plate_20230325", f) for f in os.listdir("/data/data_set/doriskao/ocr_dataset/car_plate_20230325")]
     text_results = []
 
     print('Test...')
+    st = time.time()
     for img_path in img_paths:
         # img_path = '/data/data_set/doriskao/ocr_dataset/car_plate_20230325/011a65e23a51dd846c0e698d8cf6e52a13431970.png'
         # data preprocess
         input_data, img_orig_shape, img_shape = load_detect_image(img_path)
 
-        yolo_pred = yolov5_infer.run(input_data)  # shape: (1, 16128, 6)
-        bbox_pred = nms_infer.run(yolo_pred[0][0])  # the output should be decoded (rescale to original shape)
+        detect_res = detect_infer.run(input_data)  # shape: (1, 16128, 6) / effdetd0: (1, 9, 64, 64)
+        bbox_pred = nms_infer.run(detect_res[0][0])  # the output should be decoded (rescale to original shape)
         bbox_pred = bbox_pred[0].reshape(10, 6)
 
         bbox_threshold = 0.15
@@ -84,6 +124,7 @@ if __name__ == '__main__':
         text = text.upper()
         # print('text:', text)
         text_results.append(text)
+    print('inference time', time.time() - st)
 
     # with open('/home/doriskao/project/ocr/test_result_yolov5m_53trt.json', 'w') as f:
         # res = {'filename': img_paths, 'text': text_results}
