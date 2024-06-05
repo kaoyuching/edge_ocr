9ea33574574692ea5cda030c5097ebe06cb483d4
doriskao <doris_kao@muenai.com>
2024-06-05 15:47:50 +0800

    NEW: utils functions for video capture and app config

---
+++ tensorrt/utils/base.py
@@ -0,0 +1,31 @@
+import abc
+import numpy as np
+from pydantic import BaseSettings
+
+
+class InferConfig(BaseSettings):
+    chars = '0123456789abcdefghijklmnopqrstuvwxyz'
+    bbox_threshold = 0.15
+
+    class Config:
+        case_insensitive = False
+
+
+class UserConfig(BaseSettings):
+    detect_model_path: str
+    nms_model_path: str
+    crnn_model_path: str
+
+    class Config:
+        orm_mode=True
+        env_file='.env'
+        case_insensitive = False
+
+
+class BaseInference(abc.ABC):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def run(self, input_data: np.ndarray, **kwargs):
+        raise NotImplementedError()
---
+++ tensorrt/utils/capture.py
@@ -0,0 +1,39 @@
+import cv2
+from typing import Generator
+
+
+def capture_video(video_id: int, inference_rate: float, display: bool = True) -> Generator:
+    """
+    inference_rate: number of frames to do inference per second
+    """
+    vid = cv2.VideoCapture(video_id)
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    print('FPS: ', fps)
+
+    if inference_rate > fps:
+        inference_rate = fps
+    k = int(fps / inference_rate) # FPS?
+    i = 0
+    while True:
+        i += 1
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+
+        # Display the resulting frame
+        if display:
+            cv2.imshow('frame', frame)
+
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if i % k != 0 or not ret:
+            continue
+        yield frame
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
