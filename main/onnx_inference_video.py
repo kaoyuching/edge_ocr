fa58e93c11d8599427a31f934bbfe0a70c277480
doriskao <doris_kao@muenai.com>
2024-06-07 12:46:58 +0800

    CHANGE: move add bbox to frame to image_utils.py

--- main/onnx_inference_video.py
+++ main/onnx_inference_video.py
@@ -1,18 +1,15 @@
+import cv2
 from edge_ocr.onnx.onnx_inference import onnx_inference
 from edge_ocr.utils.capture import capture_video
 from edge_ocr.utils.base import UserConfig
-from edge_ocr.utils.image_utils import extend_box
+from edge_ocr.utils.image_utils import add_bbox_to_frame
 
 
 if __name__ == '__main__':
     user_config = UserConfig('onnx.env')
-    imgsrc = capture_video(video_id=0, inference_rate=1, display=False)
-    import cv2
-    for img, bbox, res in onnx_inference(imgsrc, user_config):
-        frame = img.copy()
+    imgsrc = capture_video(video_id=0, inference_rate=30, display=False)
+    for frame, bbox, res in onnx_inference(imgsrc, user_config):
         if bbox is not None:
-            h, w, _ = frame.shape
-            bbox = [int(x) for x in extend_box(bbox, ratio=1.15, w=w, h=h)]
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+            frame = add_bbox_to_frame(frame, bbox, extend_ratio=1.15)
         cv2.imshow('frame', frame)
         print(bbox, res)
